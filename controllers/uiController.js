917f317c0007d0529a102541f88a6e38c4afd1a5
Naman Sachdeva <namansachdevaa@gmail.com>
2019-01-16 12:55:49 +0530

    promises for async usage

--- controllers/uiController.js
+++ controllers/uiController.js
@@ -10,14 +10,54 @@ exports.downloadSample = function(req,res) {
 	var fs = require('fs');
 	var pdf = require('html-pdf');
 	var path = require('path')
+	var html, options;
 
-	var html = fs.readFileSync('./uploads/templateSelected.html', 'utf8');
-	var options = { format: 'Letter' };
+	try {
+		html = fs.readFileSync('./uploads/templateSelected.html', 'utf8');
+		options = { format: 'A4' };
+	} catch(err) {
+		res.send("No Template Available.")
+	}
 
-	pdf.create(html, options).toFile('./pdfs/samplepdf.pdf', function(err, res) {
-	  if (err) return console.log(err);
-	  console.log(res);
-	});
-	// Use promise here
-	res.sendFile(path.join(__dirname, '../pdfs/samplepdf.pdf'));
+	var createPromise = new Promise(function(resolve, reject) {
+		pdf.create(html, options).toFile('./pdfs/samplepdf.pdf', function(err, res) {
+		  if(err)
+		  	reject(err);
+		  else
+		  	resolve(res)
+		});
+	})
+
+	// Use promise here - DONE
+	createPromise.then(function(result) {
+		console.log('File is - ', result);
+		res.sendFile(path.join(__dirname, '../pdfs/samplepdf.pdf'));
+	})
+}
+
+exports.sendEmails = function(req, res) {
+	var csv = require('csv');
+	var path = require('path');
+	var obj = csv();
+
+	function AttendeeData(name, email, phone) {
+	    this.Name = name;
+	    this.EmailID = email;
+	    this.PhoneNo = phone;
+	};
+
+	var Attendees = [];
+	var readCSVPromise = new Promise(function(resolve, reject) {
+		obj.from.path(path.join(__dirname, '../uploads/sheetSelected.csv')).to.array(function(data) {
+			for (var index = 0; index < data.length; index++) {
+	        	Attendees.push(new AttendeeData(data[index][0], data[index][1], data[index][2]));
+	    	}
+	    	resolve(Attendees);
+		});
+	})
+
+	readCSVPromise.then(function(result) {
+	    console.log(result);
+	    // send mail here.
+	})
 }
--- routes/uiRoutes.js
+++ routes/uiRoutes.js
@@ -1,12 +1,15 @@
 module.exports = function(app, uploadOptions) {
 	var uiControls = require('../controllers/uiController');
 
-	app.route('/')
+	app.route('/upload')
 		.get(uiControls.showHomePage);
 	
-	app.route('/upload')
+	app.route('/send')
 		.post(uploadOptions, uiControls.uploadFiles);
 
 	app.route('/sample')
-		.get(uiControls.downloadSample)
+		.get(uiControls.downloadSample);
+
+	app.route('/started')
+		.post(uiControls.sendEmails);
 }
