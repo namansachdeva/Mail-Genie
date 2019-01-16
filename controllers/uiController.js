d7ac0a1f084e6b7724bda55cf025f8bae74e0ace
Naman Sachdeva <namansachdevaa@gmail.com>
2019-01-17 00:54:59 +0530

    mails sent to last email, needs sync loop

--- controllers/uiController.js
+++ controllers/uiController.js
@@ -43,7 +43,7 @@ exports.sendEmails = function(req, res) {
 	var path = require('path');
 	var fs = require('fs');
 	var pdf = require('html-pdf');
-	
+	const nodemailer = require('nodemailer');
 	var obj = csv();
 
 	function AttendeeData(name, email, phone) {
@@ -66,16 +66,64 @@ exports.sendEmails = function(req, res) {
 
 	var htmlSource;
 	readCSVPromise.then(function(result) {
-	    console.log(result);
+
 		htmlSource = fs.readFileSync("./uploads/templateSelected.html", "utf8");
+	
 	}).then(function(){
+
 		var templateStr = htmlSource.toString();
-		var candidateName = "NAMAN SACHDEVA"
-		console.log(Attendees[0]);
+		options = { height: '676px',
+					width: '878px',
+					timeout: 30000,
+					base: 'file://' + path.join(__dirname, '../public/img/') };
+
+		// TODO make this synchronous 
 		for (var index = 0; index < num; index++) {
+	        
 	        var tempHTML = templateStr.replace("mojojojo", Attendees[index].Name)
-			console.log(tempHTML);
-			//send mail here
+			var emailP = Attendees[index].EmailID;
+
+			var createPromiseForEach = new Promise(function(resolve, reject) {
+				pdf.create(tempHTML, options).toFile('./pdfs/certificate.pdf', function(err, res) {
+				  if(err)
+				  	reject(err);
+				  else
+				  	resolve(res)
+				});
+			})
+
+			createPromiseForEach.then(function(result) {
+				const transporter = nodemailer.createTransport({
+									service: 'Gmail',
+						  			auth: {
+						    		user: 'namansachdevaa@gmail.com',
+						    		pass: 'axzs28@!'
+						 }
+					});
+				
+				console.log(emailP)
+				const options = {
+						from: 'namansachdevaa@gmail.com',
+						to: emailP,
+						subject: 'Your Android Development Workshop certificate is here!',
+						text: 'Hello World',
+						attachments: [
+						    {
+						     path: path.join(__dirname, '../pdfs/certificate.pdf')
+						    }
+						]
+					};
+
+				transporter.sendMail(options, (error, info) =>{
+					if(error) {
+						console.log('----------------------ERROR---------------------------')
+					} else {
+						console.log('----------------------DONE---------------------------')
+						console.log(info)
+					}
+				});
+			})
 	    }
 	})
+	res.send("success")
 }
